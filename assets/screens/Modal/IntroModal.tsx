e6a2d54f37bd427d555a0ca769031ff725fd4e4b
Tristan Listanco <tristanlistanco@Tristans-MacBook-Pro.local>
2021-10-31 22:22:09 +0800

    Modal WIP

--- assets/screens/Modal/IntroModal.tsx
+++ assets/screens/Modal/IntroModal.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-
 const IntroModal = () => {
-    return(
-        <View style={styles.container}>
-            <View style={{flexDirection:'column',marginTop:30}}>
-            <Text style={{fontFamily:"SFProDisplay-Bold",fontSize:20}}>
-                Practical Research 
-            </Text>
-            <Text>
-                For Beginners
-            </Text>
+	return (
+		<View style={styles.container}>
+			<View
+				style={{ flexDirection: "column", marginTop: 30, alignItems: "center" }}
+			>
+				<Text style={{ fontFamily: "SFProDisplay-Bold", fontSize: 30 }}>
+					Practical Research
+				</Text>
+				<Text style={{ fontFamily: "SFProDisplay-Bold", fontSize: 30 }}>
+					For Beginners
+				</Text>
+				<View style={{ flexDirection: "row" }}>
+					<View style={{ flexDirection: "column" }}>
 
-            </View>
-            
-        </View>
-    )
-}
+					<Text style={{ fontFamily: "SFProDisplay-Bold", fontSize: 20 }}>
+						Offline Access
+					</Text>
+					<Text style={{ fontFamily: "SFProDisplay-Medium", fontSize: 15 }}>Potangina mo Jessah ang ganda mooo.Hindi ko maexplain yung personality mo</Text>
+
+					<Text style={{ fontFamily: "SFProDisplay-Bold", fontSize: 20 }}>
+						Offline Access
+					</Text>
+					<Text style={{ fontFamily: "SFProDisplay-Medium", fontSize: 15 }}>Potangina mo Jessah ang ganda mooo.Hindi ko maexplain yung personality mo</Text>
+
+					</View>
+					
+				</View>
+			</View>
+		</View>
+	);
+};
 
 export default IntroModal;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-    },
-
-})
+	container: {
+		flex: 1,
+		paddingHorizontal:40
+	},
+});
