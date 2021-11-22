f3395e3cfa516f1149ca2b59967ff9b8a94bd5d5
TristanListanco_2004 <tannyencina@gmail.com>
2021-11-22 22:11:02 +0800

    [About] Acknowledgements and Letters now placed in flatlist
    from basic props to flatlist for easy data updates and binds

---
+++ assets/data/OthersData.tsx
@@ -0,0 +1,23 @@
+const Others = [
+  {
+    id:"1",
+    title:"Opensource License",
+    desc:"View opensource licenses used"
+  },
+  {
+    id:"2",
+    title:"Letter of Acknowledgement",
+    desc:"Acknowledgement letter from the School"
+  },
+  {
+    id:'3',
+    title:"End User License Agreement",
+    desc:"User's term and conditions in using this app"
+  },
+  {
+    id:'4',
+    title:"Memorandum of Agreement",
+    desc:"The agreement of the developer and the school"
+  },
+];
+export default Others;
