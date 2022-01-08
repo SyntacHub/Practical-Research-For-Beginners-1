65a15deddd6b9757a2304773452886d4f88321fa
TristanListanco_2004 <tannyencina@gmail.com>
2022-01-08 13:35:27 +0800

    WIP Augmented Reality for Research LabTools

---
+++ assets/screens/root/ARViewer.tsx
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import RNFS from 'react-native-fs';
+import FileViewer from 'react-native-file-viewer';
+import ProgressCircle from 'react-native-progress-circle'
